9a9ac2cd41c699dede8bd931bdef68d7857379e0
Jorge Gallardo <jorge8486@gmail.com>
2015-08-06 16:42:40 -0400

    first commit

---
+++ Memorable Places/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by Jorge Gallardo on 8/6/15.
+//  Copyright © 2015 Jorge Gallardo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var map: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
